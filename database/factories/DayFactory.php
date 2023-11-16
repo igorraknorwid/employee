7c5f04696540955c039a63f9bdcc2ec76a2f4d2f
igorraknorwid <igorrakfed@gmail.com>
2023-11-16 16:58:38 +0100

    DayModel

---
+++ database/factories/DayFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Day>
+ */
+class DayFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'day' => fake()->dayOfMonth,
+            'day_title' => fake()->dayOfWeek,
+            'week'=> fake()->number,
+            'month' => fake()->number,
+            'year' => fake()->number,
+        ];
+    }
+}
