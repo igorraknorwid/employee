6f35a98c139a253e65083ed49175a3cca7e5bbe4
igorraknorwid <igorrakfed@gmail.com>
2023-11-16 16:38:57 +0100

    fit:add User model and UserFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,27 @@ class DatabaseSeeder extends Seeder
         // \App\Models\Station::factory(4)->create();
         // \App\Models\Service::factory(8)->create();
         //\App\Models\SampleService::factory(4)->create();
-    }
+         $startDate = now()->setYear(2024)->setMonth(12)->startOfMonth();
+        
+        // Set the start date to the first day of January 2024
+        $startDate = now()->setYear(2024)->setMonth(1)->startOfMonth();
+
+        // Create records for all days of January 2024
+        while ($startDate->month == 1) {
+            \App\Models\Day::factory()->create([
+                'day' => $startDate->day,
+                'day_title' => $startDate->format('l'), // 'l' returns the full day name (e.g., Monday)
+                'week' => $startDate->weekOfMonth,
+                'month' => $startDate->month,
+                'year' => $startDate->year,
+            ]);
+
+            // Move to the next day
+            $startDate->addDay();
+        }
+        
+        }
+
+        
+      
 }
